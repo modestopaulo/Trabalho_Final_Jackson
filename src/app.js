163a4166c3c952277268ad5c4ec93598e4dc3d33
modestopaulo <112510824+modestopaulo@users.noreply.github.com>
2025-06-15 22:21:18 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.json()); // Permite que o Express leia JSON no corpo das requisições
+app.use(express.json()); 
 
 // Importar e usar as rotas
 const swaggerUi = require('swagger-ui-express');
@@ -11,10 +11,10 @@ const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
-const orderRoutes = require('./routes/orderRoutes'); // Corrigido nome do arquivo
+const orderRoutes = require('./routes/orderRoutes'); 
 
-app.use('/api/auth', authRoutes); // Rotas de autenticação (login, registro)
-app.use('/api/users', userRoutes); // Rotas de gerenciamento de usuários (perfil)
+app.use('/api/auth', authRoutes); 
+app.use('/api/users', userRoutes); 
 app.use('/api/products', productRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/orders', orderRoutes);
@@ -23,7 +23,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.get('/', (req, res) => {
     res.send('API RESTful em Node.js com Sequelize');
 });
-// Middleware para tratamento de erros 
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Algo deu errado!');
